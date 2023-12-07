2e32d0f9f3193e0201d815b21129fcbee7e4bd72
Animesh Singh <csanimeshsingh747@gmail.com>
2023-12-08 01:22:43 +0530

    Marks update

--- ums.java
+++ ums.java
@@ -106,4 +106,71 @@ class Course {
         return studentNames;
     }
 
+    public Exam conductExam(String examName) {
+        return new Exam(examName, students);
+    }
+
+    public Map<String, Integer> generateReport(Exam exam) {
+        Map<String, Integer> report = new HashMap<>();
+        for (Student student : students) {
+            int marks = exam.getMarks(student);
+            report.put(student.name, marks);
+        }
+        return report;
+    }
+}
+
+class Exam {
+    private String name;
+    private Map<Student, Integer> marks;
+
+    public Exam(String name, List<Student> students) {
+        this.name = name;
+        this.marks = new HashMap<>();
+        for (Student student : students) {
+            marks.put(student, null);
+        }
+    }
+
+    public void assignMarks(Student student, int marks) {
+        if (this.marks.containsKey(student)) {
+            this.marks.put(student, marks);
+        }
+    }
+
+    public int getMarks(Student student) {
+        return marks.get(student);
+    }
+}
+
+public class ums {
+    public static void main(String[] args) {
+
+        Student student1 = new Student(1, "Animesh", 22, "Male");
+        Student student2 = new Student(2, "Avinash", 21, "Male");
+
+        Teacher teacher1 = new Teacher(101, "Dhruv jain", 35, "Male");
+
+        Course course1 = new Course("CS1011", "Introduction to OOPS");
+        Course course2 = new Course("CS1012", "Advanced Concepts");
+
+        student1.registerCourse(course1);
+        student2.registerCourse(course1);
+
+        teacher1.assignCourse(course1);
+
+        System.out.println(student1.displayCourses());
+        System.out.println(teacher1.displayCoursesTaught());
+
+        Exam exam1 = course1.conductExam("Midterm Exam");
+        exam1.assignMarks(student1, 90);
+        exam1.assignMarks(student2, 85);
+
+        Exam exam2 = course2.conductExam("Finalterm Exam");
+        exam2.assignMarks(student1, 70);
+        exam2.assignMarks(student2, 95);
+
+        Map<String, Integer> report = course1.generateReport(exam1);
+        System.out.println(report);
+    }
 }
